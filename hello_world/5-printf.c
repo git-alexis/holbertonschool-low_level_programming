80f3c0253ced58bcd4bafc8e32e140379806422b
git-alexis <alex59.billemont@gmail.com>
2023-10-15 14:00:03 -0700

    Hello, printf

--- hello_world/5-printf.c
+++ hello_world/5-printf.c
@@ -3,13 +3,15 @@
 /**
  * main - Entry point
  *
- * Description: 'This function prints a sample message using the printf function * and return 0 to indicate successful execution.'
+ * Description: 'This function prints a sample message using
+ * the printf function * and return 0 to indicate
+ * successful execution.'
  *
  * Return: Always 0 (Success)
  */
 
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,");
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
