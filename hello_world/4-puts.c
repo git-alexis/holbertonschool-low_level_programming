d6ce4d45ec26ad549b412742f35db1e82cea5ced
git-alexis <alex59.billemont@gmail.com>
2023-10-15 13:27:08 -0700

    test

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,13 +1,11 @@
 #include <stdio.h>
 /**
- * Function: puts
+ * Function: "puts"
  *
- * Description:
- * 'This function prints a sample message using the puts function
- * and returns 0 to indicate successful execution.'
+ * Description: "This function prints a sample message using the puts function
+ * and returns 0 to indicate successful execution."
  *
- * Returns:
- * 0 - Indicating successful execution.
+ * Returns: 0 - "Indicating successful execution."
  */
 int main(void)
 {
