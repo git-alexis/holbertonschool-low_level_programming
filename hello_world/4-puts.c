42f69c57a3dfac800870b7578275650ef9fc7aa2
git-alexis <alex59.billemont@gmail.com>
2023-10-15 13:34:52 -0700

    test

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,12 +1,15 @@
 #include <stdio.h>
+
 /**
  * main - Entry point
  *
  * Description: 'This function prints a sample message using the puts function
  * and returns 0 to indicate successful execution.'
+ * @parameter: void
  *
  * Returns: Always 0 (Success)
  */
+
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
