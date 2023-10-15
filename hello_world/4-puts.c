0f5abf6f0c75f4b483d25bca98703612214e375c
git-alexis <alex59.billemont@gmail.com>
2023-10-15 13:37:06 -0700

    test

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -4,10 +4,9 @@
  * main - Entry point
  *
  * Description: 'This function prints a sample message using the puts function
- * and returns 0 to indicate successful execution.'
- * @parameter: void
+ * and returns 0 to indicate successful execution.
  *
- * Returns: Always 0 (Success)
+ * Returns: 'Always 0 (Success)'
  */
 
 int main(void)
