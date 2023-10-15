912a9fda980af6f480a1ace2f2d37929c70fcdff
git-alexis <alex59.billemont@gmail.com>
2023-10-15 13:24:15 -0700

    test

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -3,8 +3,8 @@
  * Function: puts
  *
  * Description:
- * This function prints a sample message using the puts function
- * and returns 0 to indicate successful execution.
+ * 'This function prints a sample message using the puts function
+ * and returns 0 to indicate successful execution.'
  *
  * Returns:
  * 0 - Indicating successful execution.
