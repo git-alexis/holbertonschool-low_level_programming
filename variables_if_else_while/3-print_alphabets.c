cef8b6360793adec1f65f68a85ae590bd3b83916
git-alexis <alex59.billemont@gmail.com>
2023-10-16 07:30:24 -0700

    alphaBET

---
+++ variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: 'This function prints the alphabet in lowercase
+ * then in uppercase and return 0 to indicate successful
+ * execution.'
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+		putchar(letter);
+	for (letter = 'A'; letter <= 'z'; letter++)
+		putchar(letter);
+	putchar('\n');
+
+	return (0);
+}
