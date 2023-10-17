509af78ca18a3eaf27db10ef560652dc70519b25
git-alexis <alex59.billemont@gmail.com>
2023-10-17 02:24:53 -0700

    reversed alphabet

---
+++ variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: 'This function prints the alphabet in lowercase in reversed
+ * and return 0 to indicate successful execution.'
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char letter;
+
+	for (letter = 'z'; letter >= 'a'; letter--)
+		putchar(letter);
+	putchar('\n');
+
+	return (0);
+}
