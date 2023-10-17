177bf94f47ce63e31ced22dbf2c6df415e58652e
git-alexis <alex59.billemont@gmail.com>
2023-10-17 07:34:41 -0700

    putchar + main.h

---
+++ functions_nested_loops/0-putchar.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "main.h" 
+
+/**
+ * main - Entry point
+ *
+ * Description: 'This function prints the message _putchar and
+ * return 0 to indicate successful execution'
+ */
+
+int main(void)
+{
+	puts("_putchar");
+	return (0);
+}
