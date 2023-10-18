7fef4d641855cd41bc18e06e9bfff1abc4cb5c11
git-alexis <alex59.billemont@gmail.com>
2023-10-18 01:57:51 -0700

    print _putchar

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -1,17 +1,24 @@
-#include <stdio.h>
 #include "main.h"
 
 /**
  * main - Entry point
  *
- * Description: 'This function prints the message _putchar and
- * return 0 to indicate successful execution'
+ * Description: 'This function prints the message _putchar with the _putchar
+ * function and return 0 to indicate successful execution'
  *
  * Return: Always 0 (Success)
  */
 
 int main(void)
 {
-	puts("_putchar");
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
 	return (0);
 }
