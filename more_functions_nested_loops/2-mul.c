35edc85c03f554fedec6a8d12003c66e3d380077
git-alexis <alex59.billemont@gmail.com>
2023-10-21 08:07:08 -0700

    Multiplication function

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * mul - Multiplication of number
+ *
+ * Description: 'This function prints nothing and return the product
+ * of an multiplication of two number'
+ *
+ * @a: Integer variable
+ * @b: Integer variable
+ *
+ * Return: Integer (The product)
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
