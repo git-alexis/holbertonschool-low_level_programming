d678eb561d2e84312616712aeda7ba9d43e3621e
git-alexis <alex59.billemont@gmail.com>
2023-10-21 01:41:17 -0700

    return result of addition

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * add - addition of number
+ *
+ * Description: 'This function prints nothing and return the result
+ * of an addition of two number'
+ *
+ * @num1: Integer variable
+ * @num2: Integer variable
+ *
+ * Return: Integer (The sum)
+ */
+
+int add(int num1, int num2)
+{
+	return (num1 + num2);
+}
