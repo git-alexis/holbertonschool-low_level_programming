9729afd57611023d0d603b1ad0e5f96538a2cb2a
git-alexis <alex59.billemont@gmail.com>
2023-10-23 06:48:50 -0700

    update to 98

---
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * reset_to_98 - Update to 98
+ *
+ * Description: 'This function updapte with the address of integer
+ * his value to 98 return nothing'
+ *
+ * @n: Address of integer variable
+ *
+ * Return: Nothing
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
