7313ac7011588fc45506d0dd1f71c3177cb4a671
git-alexis <alex59.billemont@gmail.com>
2023-10-25 07:00:25 -0700

    test

---
+++ pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _strcpy - Print n element of array
+ *
+ * Description: 'This function print the n first elements of array of
+ * integer followed by a new line and return nothing'
+ *
+ * @dest: Pointer of array of integer a
+ * @src: Integer variable
+ *
+ * Return: Nothing
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	dest = src;
+	return (dest);
+}
