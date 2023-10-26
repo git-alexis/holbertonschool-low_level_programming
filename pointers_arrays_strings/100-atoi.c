7e3c7643c4756569540690ea968230d6795d506d
git-alexis <alex59.billemont@gmail.com>
2023-10-26 07:55:59 -0700

    Convert a string to an integer

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -14,20 +14,33 @@
 
 int _atoi(char *s)
 {
-	int index = 0;
-	int sign = 1;
-	int result = 0;
+	int length = 0;
 
-	for (; s[index] < '0' || s[index] > '9'; index++)
+	for (; (s[length] < '0' || s[length] > '9') && s[length] != '\0';)
 	{
-		if (s[index] == '-')
+		length++;
+	}
+	if (length == 0 || s[length] == '\0')
+	{
+		return (0);
+	}
+	else
+	{
+		int index = 0;
+		int sign = 1;
+		int result = 0;
+
+		for (; s[index] < '0' || s[index] > '9'; index++)
 		{
-			sign = -1 * sign;
+			if (s[index] == '-')
+			{
+				sign = -1 * sign;
+			}
 		}
+		for (; s[index] >= '0' && s[index] <= '9'; index++)
+		{
+			result = 10 * result + (s[index] - '0');
+		}
+		return(sign * result);
 	}
-	for (; s[index] >= '0' && s[index] <= '9'; index++)
-	{
-		result = 10 * result + (s[index] - '0');
-	}
-	return(sign * result);
 }
