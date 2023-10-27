a2cf7df69597842f28315b46f791f0d6ffe955c5
git-alexis <alex59.billemont@gmail.com>
2023-10-27 01:28:31 -0700

    version sans cas 0 int_min_max

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -14,32 +14,20 @@
 
 int _atoi(char *s)
 {
-	int length = 0;
+	int index = 0;
+	int sign = 1;
+	int result = 0;
 
-	for (; (s[length] < '0' || s[length] > '9') && s[length] != '\0'; length++)
+	for (; s[index] < '0' || s[index] > '9'; index++)
 	{
-	}
-	if (length == 0 || s[length] == '\0')
-	{
-		return (0);
-	}
-	else
-	{
-		int index = 0;
-		int sign = 1;
-		int result = 0;
-
-		for (; s[index] < '0' || s[index] > '9'; index++)
+		if (s[index] == '-')
 		{
-			if (s[index] == '-')
-			{
-				sign = -1 * sign;
-			}
+			sign = -1 * sign;
 		}
-		for (; s[index] >= '0' && s[index] <= '9'; index++)
-		{
-			result = 10 * result + (s[index] - '0');
-		}
-		return(sign * result);
 	}
+	for (; s[index] >= '0' && s[index] <= '9'; index++)
+	{
+		result = 10 * result + (s[index] - '0');
+	}
+	return(sign * result);
 }
