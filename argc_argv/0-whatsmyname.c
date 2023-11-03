9cc91f40d44d0a57b80189407a453eaeb9e42193
git-alexis <alex59.billemont@gmail.com>
2023-11-03 08:45:04 -0700

    doc modification

--- argc_argv/0-whatsmyname.c
+++ argc_argv/0-whatsmyname.c
@@ -6,8 +6,8 @@
  * Description: 'This function prints its name followed by a new line
  * and return always 0 to indicate successful operation'
  *
- * argc: Integer variable
- * argv: Arrays of character
+ * @argc: Integer variable
+ * @argv: Arrays of character
  *
  * Return: Always 0 (Success)
  */
--- argc_argv/1-args.c
+++ argc_argv/1-args.c
@@ -6,8 +6,8 @@
  * Description: 'This function prints the number of arguments passed into it
  * followed by a new line and return always 0 to indicate successful operation'
  *
- * argc: Integer variable
- * argv: Arrays of character
+ * @argc: Integer variable
+ * @argv: Arrays of character
  *
  * Return: Always 0 (Success)
  */
