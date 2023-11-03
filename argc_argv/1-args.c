55a58a33115f91b7b22635bc509d94524d8adf1d
git-alexis <alex59.billemont@gmail.com>
2023-11-03 08:41:07 -0700

    prints the number of arguments passed into it

---
+++ argc_argv/1-args.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: 'This function prints the number of arguments passed into it
+ * followed by a new line and return always 0 to indicate successful operation'
+ *
+ * argc: Integer variable
+ * argv: Arrays of character
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(int argc, __attribute__((unused))char *argv[])
+{
+	printf("%d\n", argc - 1);
+	return (0);
+}
