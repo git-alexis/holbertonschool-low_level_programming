09aaebca814ffedd2a8948f4611e664b6e8342a1
git-alexis <alex59.billemont@gmail.com>
2023-11-13 07:50:59 -0800

    creates a new dog

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -25,11 +25,11 @@ int _strlen(char *array)
 }
 
 /**
- * new_dog - creates a new dog
+ * new_dog - Creates a new dog
  *
  * Description: 'This function creates a new dog and initialise it.
  * She returns the pointer of the struct or Null if insufficient memory
- * was available'
+ * was available and frees the doc_t struct'
  *
  * @name: Char pointer
  * @age: Float variable
@@ -48,35 +48,35 @@ dog_t *new_dog(char *name, float age, char *owner)
 
 	new_dog = malloc(sizeof(dog_t));
 	if (new_dog == NULL)
-	{
 		return (NULL);
-	}
 
 	new_dog->name = malloc(length_name * sizeof(char) + 1);
 	if (new_dog->name == NULL)
 	{
 		return (NULL);
+		free(new_dog);
 	}
 
 	new_dog->owner = malloc(length_owner * sizeof(char) + 1);
 	if (new_dog->owner == NULL)
 	{
 		return (NULL);
+		free(new_dog->name);
+		free(new_dog);
 	}
 
 	for (index = 0; index < length_name; index++)
-	{
 		new_dog->name[index] = name[index];
-	}
 	new_dog->name[index] = '\0';
 
 	new_dog->age = age;
 
 	for (index = 0; index < length_owner; index++)
-	{
 		new_dog->owner[index] = owner[index];
-	}
 	new_dog->owner[index] = '\0';
 
 	return (new_dog);
+	free(new_dog->owner);
+	free(new_dog->name);
+	free(new_dog);
 }
