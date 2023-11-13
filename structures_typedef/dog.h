458544d1a730c2a1e25478a01b282fb459c904b1
git-alexis <alex59.billemont@gmail.com>
2023-11-13 02:17:18 -0800

    hearder

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -23,10 +23,37 @@ struct dog
 
 #endif
 
+#ifndef INIT_DOG_PROTO
+#define INIT_DOG_PROTO
+
 void init_dog(struct dog *d, char *name, float age, char *owner);
+
+#endif
+
+#ifndef PRINT_DOG_PROTO
+#define PRINT_DOG_PROTO
+
 void print_dog(struct dog *d);
+
+#endif
+
+#ifndef DOG_T_STRUCT
+#define DOG_T_STRUCT
 
 typedef struct dog dog_t;
 
+#endif
+
+#ifndef NEW_DOG_PROTO
+#define NEW_DOG_PROTO
+
 dog_t *new_dog(char *name, float age, char *owner);
+
+#endif
+
+#ifndef FREE_DOG_PROTO
+#define FREE_DOG_PROTO
+
 void free_dog(dog_t *d);
+
+#endif
