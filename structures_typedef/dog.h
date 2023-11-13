1efb0a48da05ef59c4f8dac5dd42dd495eb677fa
git-alexis <alex59.billemont@gmail.com>
2023-11-13 01:52:11 -0800

    Define a new type struct dog

---
+++ structures_typedef/dog.h
@@ -0,0 +1,24 @@
+#ifndef DOG_STRUCT
+#define DOG_STRUCT
+
+/**
+ * struct dog - Entry point
+ *
+ * Description: 'This new struct gives the name, age and owner of a dog
+ * and return nothing'
+ *
+ * @name: Char pointer
+ * @age: Float
+ * @owner: Char pointer
+ *
+ * Return: Nothing
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
