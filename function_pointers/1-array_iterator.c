be0474f53de3990dde33d710411c93211b4bfa3f
git-alexis <alex59.billemont@gmail.com>
2023-11-14 01:42:44 -0800

    executes a function given as a parameter on each element of an array

---
+++ function_pointers/1-array_iterator.c
@@ -0,0 +1,27 @@
+#include <stdlib.h>
+
+/**
+ * array_iterator - Print each element of an array
+ *
+ * Description: 'This function displays by executing a function given as
+ * a parameter on each element of an array. She return nothing'
+ *
+ * @array: Integer array
+ * @size: size_t variable
+ * @action: action funtion pointer
+ *
+ * Return: Nothing
+ */
+
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	size_t index;
+
+	if (action != NULL)
+	{
+		for (index = 0; index < size; index++)
+		{
+			action(array[index]);
+		}
+	}
+}
