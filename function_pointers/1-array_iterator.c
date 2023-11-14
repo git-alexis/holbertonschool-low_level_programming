1541c38c516537435ff05da22ea2bb05d22deb7a
git-alexis <alex59.billemont@gmail.com>
2023-11-14 01:46:22 -0800

    executes a function given as a parameter on each element of an array

--- function_pointers/1-array_iterator.c
+++ function_pointers/1-array_iterator.c
@@ -17,7 +17,7 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	size_t index;
 
-	if (action != NULL)
+	if (array != NULL && action != NULL)
 	{
 		for (index = 0; index < size; index++)
 		{
