f9c841c79867ba72c4965bf15fdb84c2f2278711
git-alexis <alex59.billemont@gmail.com>
2023-11-17 06:33:19 -0800

    prints numbers, followed by a new line

---
+++ variadic_functions/1-print_numbers.c
@@ -0,0 +1,40 @@
+#include <stdarg.h>
+#include <stdio.h>
+
+/**
+ * print_numbers - prints numbers
+ *
+ * Description: 'This function prints numbers separated by
+ * the string separator, followed by a new line and return nothing'
+ *
+ * @separator: Constant string variable
+ * @n: Constant unsigned integer variable
+ *
+ * Return: Nothing
+ */
+
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	unsigned int index;
+	va_list print_arg;
+
+	va_start(print_arg, n);
+
+	for (index = 0; index < n; index++)
+	{
+		printf("%d", va_arg(print_arg, int));
+
+		if (separator != NULL && index < n - 1)
+		{
+			printf("%s", separator);
+		}
+		else
+		{
+			printf("\n");
+		}
+	}
+
+	va_end(print_arg);
+
+	printf("\n");
+}
