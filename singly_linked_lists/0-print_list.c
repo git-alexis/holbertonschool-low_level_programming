2b3fa8fc6303e4db23bf5faa8b8f59bc07267e3a
git-alexis <alex59.billemont@gmail.com>
2023-11-27 18:00:03 +0100

    header file with struct list_s and proto + function to prints all the elements of a list_t list

---
+++ singly_linked_lists/0-print_list.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+
+size_t print_list(const list_t *h)
+{
+	size_t node_count = 0;
+
+	while (h != NULL)
+	{
+		if (h->str == NULL)
+		{
+			printf("[0] (nil)\n");
+		}
+		else
+		{
+			printf("[%u] %s\n", h->len, h->str);
+		}
+
+		h = h->next;
+		node_count++;
+	}
+
+	return node_count;
+}
