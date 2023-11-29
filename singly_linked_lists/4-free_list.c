f0dd58a7b3122f1ed47c7f811871cf0f28700bbf
git-alexis <alex59.billemont@gmail.com>
2023-11-29 15:47:42 +0100

    betty code rectification

--- singly_linked_lists/4-free_list.c
+++ singly_linked_lists/4-free_list.c
@@ -5,10 +5,10 @@ void free_list(list_t *head)
 	list_t *current_node = head;
 	list_t *next_node;
 
-    while (current_node != NULL)
+	while (current_node != NULL)
 	{
-        next_node = current_node->next;
-        free(current_node);
-        current_node = next_node;
-    }
+		next_node = current_node->next;
+		free(current_node);
+		current_node = next_node;
+	}
 }
