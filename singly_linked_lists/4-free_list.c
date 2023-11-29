8d349dc7ccd6b7c49729485ebdf73ebc38fad455
git-alexis <alex59.billemont@gmail.com>
2023-11-29 15:45:55 +0100

    frees a list_t list + add prototype in hearder

---
+++ singly_linked_lists/4-free_list.c
@@ -0,0 +1,14 @@
+#include "lists.h"
+
+void free_list(list_t *head)
+{
+	list_t *current_node = head;
+	list_t *next_node;
+
+    while (current_node != NULL)
+	{
+        next_node = current_node->next;
+        free(current_node);
+        current_node = next_node;
+    }
+}
