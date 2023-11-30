7a0ba879a29ed277aac8fe50a6486db58f4affa1
git-alexis <alex59.billemont@gmail.com>
2023-11-30 17:33:12 +0100

    returns the nth node of a dlistint_t linked list

--- doubly_linked_lists/5-get_dnodeint.c
+++ doubly_linked_lists/5-get_dnodeint.c
@@ -2,15 +2,15 @@
 
 dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 {
-    unsigned int position = 0;
+	unsigned int position = 0;
 	dlistint_t *temp_point = head;
 
-    while (temp_point != NULL)
-    {
-        if (position == index)
-            return (temp_point);
-        temp_point = temp_point->next;
-        position++;
-    }
-    return (NULL);
+	while (temp_point != NULL)
+	{
+		if (position == index)
+			return (temp_point);
+		temp_point = temp_point->next;
+		position++;
+	}
+	return (NULL);
 }
