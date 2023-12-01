e520dc8ffb99ef163fef5390844dc6a280460f04
git-alexis <alex59.billemont@gmail.com>
2023-12-01 15:07:09 +0100

    inserts a new node at a given position

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -3,38 +3,47 @@
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
 	unsigned int position = 0;
-	dlistint_t *temp_point = *h;
+	dlistint_t *temp_point;
 	dlistint_t *new_node;
+
+	if (h == NULL)
+		{
+			return (NULL);
+		}
+
+	if (idx == 0)
+	{
+		return (add_dnodeint(h, n));
+	}
 
 	new_node = malloc(sizeof(dlistint_t));
 	if (new_node == NULL)
 		return (NULL);
 
 	new_node->n = n;
+	temp_point = *h;
 
-	while (temp_point != NULL)
+	while (temp_point != NULL && position < idx)
 	{
-		if (position == idx)
-		{
-			new_node->next = temp_point;
-			new_node->prev = temp_point->prev;
-
-			if (new_node->next != NULL)
-			{
-				temp_point->prev = new_node;
-			}
-			if (new_node->prev != NULL)
-			{
-				new_node->prev->next = new_node;
-			}
-
-			return (new_node);
-		}
-
 		temp_point = temp_point->next;
 		position++;
 	}
 
-	free(new_node);
-	return (NULL);
+	if (temp_point == NULL && position < idx)
+	{
+		free(new_node);
+		return (NULL);
+	}
+
+	if (temp_point == NULL)
+	{
+		return (add_dnodeint_end(h, n));
+	}
+	
+	new_node->next = temp_point;
+	new_node->prev = temp_point->prev;
+	temp_point->prev = new_node;
+	new_node->prev->next = new_node;
+
+	return (new_node);
 }
