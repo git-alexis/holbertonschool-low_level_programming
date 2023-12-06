e02b64def644fbc4f76ee3a285bb656fceb03243
git-alexis <alex59.billemont@gmail.com>
2023-12-06 16:17:18 +0100

    copies the content of a file to another file

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -26,7 +26,7 @@ int main(int argc, char *argv[])
 
 	file_destination = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
 	if (file_destination == -1)
-		print_error(99, "Error: Can't write to file %s\n", file_to);
+		print_error(99, "Error: Can't write to %s\n", file_to);
 
 	while ((caracs_read = read(file_source, buffer, 1024)) != -1)
 	{
@@ -35,16 +35,16 @@ int main(int argc, char *argv[])
 		if (caracs_write != caracs_read)
 			print_error(99, "Error: Can't write to file %s\n", file_to);
 	}
-
 	if (caracs_read == -1)
 		print_error(98, "Error: Can't read from file %s\n", file_from);
 
-	if (close(file_source) == -1) {
+	if (close(file_source) == -1)
+	{
 		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_source);
 		exit(100);
 	}
-
-	if (close(file_destination) == -1) {
+	if (close(file_destination) == -1)
+	{
 		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_destination);
 		exit(100);
 	}
