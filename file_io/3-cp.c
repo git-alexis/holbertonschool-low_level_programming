b5271e920b519a3738c00a80831138a2b60f5543
git-alexis <alex59.billemont@gmail.com>
2023-12-06 17:47:16 +0100

    copies the content of a file to another file

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -1,15 +1,15 @@
 #include "main.h"
 
 /**
-* main - entry point
-* @argc: arguments
-* @argv: arguments number
-* Return: Always 0
-*/
+ * main - Entry point
+ * @argc: Number of arguments
+ * @argv: Liste of char pointer arguments
+ * Return: Always 0
+ */
 
-int main(int argc, char **argv)
+int main(int argc, char *argv[])
 {
-	int source, destination, readFile, writeFile;
+	int file_source, file_destination, caracs_read, caracs_write;
 	char buffer[1024];
 
 	if (argc != 3)
@@ -18,43 +18,50 @@ int main(int argc, char **argv)
 		exit(97);
 	}
 
-	/* Ouvrir le fichier source */
-	source = open(argv[1], O_RDONLY);
-	if (source == -1)
+	file_source = open(argv[1], O_RDONLY);
+
+	if (file_source == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 		exit(98);
 	}
 
-	/* Création du fichier de destination */
-	destination = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
-	if (destination == -1)
+	file_destination = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
+
+	if (file_destination == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
 		exit(99);
 	}
 
-	/* Copie depuis source dans destination */
-	while ((readFile = read(source, buffer, sizeof(buffer))) > 0)
+	while ((caracs_read = read(file_source, buffer, sizeof(buffer))) != -1)
 	{
-		writeFile = write(destination, buffer, readFile);
-		if (writeFile != readFile)
+		caracs_write = write(file_destination, buffer, caracs_read);
+
+		if (caracs_write != caracs_read)
 		{
 			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
 			exit(99);
 		}
 	}
 
-	if (readFile == -1)
+	if (caracs_read == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 		exit(98);
 	}
 
-	if (close(source) == -1 || close(destination) == -1)
+	if (close(file_destination) == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", destination);
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_destination);
 		exit(100);
 	}
+
+	if (close(file_source) == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_source);
+		exit(100);
+	}
+
 	return (0);
 }
