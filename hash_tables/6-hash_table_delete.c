c9fe0f3f7f57fab10fd2cd4c83c9cb80858648e5
git-alexis <alex59.billemont@gmail.com>
2023-12-08 21:53:24 +0100

    deletes a hash table

--- hash_tables/6-hash_table_delete.c
+++ hash_tables/6-hash_table_delete.c
@@ -1,6 +1,9 @@
 #include "hash_tables.h"
 
-/***/
+/**
+ * hash_table_delete - function that deletes a hash table
+ * @ht: hash table
+ */
 
 void hash_table_delete(hash_table_t *ht)
 {
@@ -21,4 +24,7 @@ void hash_table_delete(hash_table_t *ht)
 			free(temp);
 		}
 	}
+
+	free(ht->array);
+	free(ht);
 }
