35dc52f16e7cf1e1e0e8e6670ae2fc1cbf5572c8
git-alexis <alex59.billemont@gmail.com>
2023-12-08 22:51:14 +0100

    prints a hash table

--- hash_tables/5-hash_table_print.c
+++ hash_tables/5-hash_table_print.c
@@ -23,12 +23,12 @@ void hash_table_print(const hash_table_t *ht)
 
 		while (current != NULL)
 		{
-			printf("'%s': '%s'", current->key, current->value);
-
-			if (current->next != NULL)
+			if (index < 0)
 			{
 				printf(", ");
 			}
+
+			printf("'%s': '%s'", current->key, current->value);
 
 			current = current->next;
 		}
