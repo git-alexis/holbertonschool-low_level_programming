4174daf5b1af2356861350081c588f394c4271a1
git-alexis <alex59.billemont@gmail.com>
2023-12-08 15:54:47 +0100

    adds an element to the hash table

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -2,15 +2,21 @@
 
 /***/
 
-hash_node_t *hash_node_creation(const char *key, const char *value)
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 	hash_node_t *new_node;
+	unsigned long int index;
+
+	if (ht == NULL || key == NULL || value == NULL)
+	{
+		return (0);
+	}
 
 	new_node = malloc(sizeof(hash_node_t));
 
 	if (new_node == NULL)
 	{
-		return (NULL);
+		return (0);
 	}
 
 	new_node->key = strdup(key);
@@ -18,7 +24,7 @@ hash_node_t *hash_node_creation(const char *key, const char *value)
 	if (new_node->key == NULL)
 	{
 		free(new_node);
-		return (NULL);
+		return (0);
 	}
 
 	new_node->value = strdup(value);
@@ -27,50 +33,13 @@ hash_node_t *hash_node_creation(const char *key, const char *value)
 	{
 		free(new_node->key);
 		free(new_node);
-		return (NULL);
-	}
-
-	return (new_node);
-}
-
-/***/
-
-int hash_table_set(hash_table_t *ht, const char *key, const char *value)
-{
-	hash_node_t *new_node, *temp;
-	unsigned long int index;
-
-	if (ht == NULL || key == NULL || value == NULL)
-	{
-		return (0);
-	}
-
-	new_node = hash_node_creation(key, value);
-
-	if (new_node == NULL)
-	{
 		return (0);
 	}
 
 	index = key_index((const unsigned char *)key, ht->size);
 
-	if (ht->array[index] == NULL)
-	{
-		new_node->next = NULL;
-		ht->array[index] = new_node;
-	}
-	else
-	{
-		temp = ht->array[index];
-
-		while (temp->next != NULL)
-		{
-			temp = temp->next;
-		}
-
-		temp->next = new_node;
-		new_node->next = NULL;
-	}
+	new_node->next = ht->array[index];
+	ht->array[index] = new_node;
 
 	return (1);
 }
