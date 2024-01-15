bd95a8688b31c9ac1fb520a6359a0e9dec08ac36
git-alexis <alex59.billemont@gmail.com>
2024-01-15 14:52:12 +0100

    add hearder with prototypes and include

---
+++ search_algorithms/search_algos.h
@@ -0,0 +1,12 @@
+#ifndef VARIA_FUNC_PROTO
+#define VARIA_FUNC_PROTO
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+int linear_search(int *array, size_t size, int value);
+int binary_search(int *array, size_t size, int value);
+
+#endif
